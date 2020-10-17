4ddd2796c1b2b44ef56b1dfd0f07fdc1932469d9
vigneashvicky <60172603+vigneashvicky@users.noreply.github.com>
2020-10-17 23:16:47 +0530

    Update settings.py

--- Test_Task/settings.py
+++ Test_Task/settings.py
@@ -89,11 +89,11 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10 mb
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.mysql',
-       'NAME': 'Task_Schema',
-       'USER': 'root',
-       'PASSWORD': 'Vsolv@123',
-       'HOST': 'localhost',
-       'PORT': '3360',
+       'NAME': '',
+       'USER': '',
+       'PASSWORD': '',
+       'HOST': '',
+       'PORT': '',
    }
 }
 
